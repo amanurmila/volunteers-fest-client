1dd6c6251af531ee8084250ee4284eeb5779f398
Md Amanullah <muhammadamanullah6001@gmail.ccom>
2024-12-22 23:39:31 +0600

    all setup done step by step

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,72 @@
+import { useContext } from "react";
+import { AuthContext } from "../providers/AuthProvider";
+import { Link } from "react-router-dom";
+const Navbar = () => {
+  const { user, logOut } = useContext(AuthContext);
+  return (
+    <div className="navbar bg-[#7E5CAD] shadow-sm px-20 text-white">
+      <div className="flex-1">
+        <Link to="/" className="flex gap-2 items-center">
+          <span className="font-bold">Volunteers Fest</span>
+        </Link>
+      </div>
+      <div className="flex-none">
+        <ul className="menu menu-horizontal px-1">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/jobs">All Volunteers</Link>
+          </li>
+
+          {!user && (
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+          )}
+        </ul>
+
+        {user && (
+          <div className="dropdown dropdown-end z-50">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost btn-circle avatar"
+            >
+              <div title={user?.displayName} className="w-10 rounded-full">
+                <img
+                  referrerPolicy="no-referrer"
+                  alt="User Profile Photo"
+                  src={user?.photoURL}
+                />
+              </div>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-gray-500 rounded-box w-52"
+            >
+              <li>
+                <Link to="/add-volunteer" className="justify-between">
+                  Add Volunteer
+                </Link>
+              </li>
+              <li>
+                <Link to="/manage-volunteers">Manage My Post</Link>
+              </li>
+              <li className="mt-2">
+                <button
+                  onClick={logOut}
+                  className="bg-gray-200 text-black block text-center"
+                >
+                  Logout
+                </button>
+              </li>
+            </ul>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/routes/Route.jsx
@@ -0,0 +1,42 @@
+import { createBrowserRouter } from "react-router-dom";
+import Error from "../pages/Error";
+import Home from "../pages/Home";
+import MainLayout from "../layout/MainLayout";
+import Login from "../components/Login";
+import Register from "../components/Register";
+
+const routes = [
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/register",
+        element: <Register />,
+      },
+    ],
+  },
+  {
+    path: "*",
+    element: <Error></Error>,
+  },
+];
+const router = createBrowserRouter(routes, {
+  future: {
+    v7_fetcherPersist: true,
+    v7_normalizeFormMethod: true,
+    v7_partialHydration: true,
+    v7_relativeSplatPath: true,
+    v7_skipActionErrorRevalidation: true,
+  },
+});
+
+export default router;
